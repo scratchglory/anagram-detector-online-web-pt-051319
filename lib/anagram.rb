265c7b1ac264ad86917fc3e7c0bf13f49cf01d57
Adella <adellakenn@gmail.com>
2019-06-05 14:58:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word 
+  
+  def initialize (array_words)
+    @array_words = array_words
+  end
+  
+# compare each word of that array to the word that the class is initialized with 
+# 
+  def match(array_words)
+    binding.pry
+    match.each do |word|
+      
+      if word == array_words
+        puts word
+      end
+    end
+  end
+  
+  
+  
+end
