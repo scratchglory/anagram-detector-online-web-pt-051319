fc168d75ae5e531d50c39d31ca6a55ddecf5e3a7
Adella <adellakenn@gmail.com>
2019-06-05 15:31:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word 
+  
+  def initialize (word)
+    @word = word
+  end
+  
+# compare each word of that array to the word that the class is initialized with 
+# 
+  def match(array_words)
+    # binding.pry
+    
+    array_words.filter do |word_split|
+    # check if word_split is an anagram of our instance
+    # binding.pry
+    
+      word_split.split("").sort == @word.split("").sort
+       
+    end
+  end
+  
+  
+  
+end
